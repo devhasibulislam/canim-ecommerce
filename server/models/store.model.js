06de84f86529c98bdfabe7dc65cdbb0c9d2e3ee9
devhasibulislam <devhasibulislam@gmail.com>
2023-11-14 19:48:00 +0600

    store schema model init

---
+++ server/models/store.model.js
@@ -0,0 +1,127 @@
+/**
+ * Title: Write a program using JavaScript on Store Model
+ * Author: Hasibul Islam
+ * Portfolio: https://devhasibulislam.vercel.app
+ * Linkedin: https://linkedin.com/in/devhasibulislam
+ * GitHub: https://github.com/devhasibulislam
+ * Facebook: https://facebook.com/devhasibulislam
+ * Instagram: https:/instagram.com/devhasibulislam
+ * Twitter: https://twitter.com/devhasibulislam
+ * Pinterest: https://pinterest.com/devhasibulislam
+ * WhatsApp: https://wa.me/8801906315901
+ * Telegram: devhasibulislam
+ * Date: 11, November 2023
+ */
+
+/* external imports */
+const mongoose = require("mongoose");
+const { ObjectId } = mongoose.Schema.Types;
+const validator = require("validator");
+
+/* create store schema */
+const storeSchema = new mongoose.Schema(
+  {
+    // for title
+    title: {
+      type: String,
+      required: [true, "Please, provide a valid store name"],
+      trim: true,
+      unique: [true, "Same store already exists"],
+    },
+
+    // for description
+    description: {
+      type: String,
+      required: [true, "Please, provide store description"],
+      trim: true,
+    },
+
+    // for thumbnail
+    thumbnail: {
+      url: {
+        type: String,
+        validate: [validator.isURL, "Please provide a valid thumbnail URL"],
+        default: "https://placehold.co/296x200.png",
+      },
+      public_id: {
+        type: String,
+        default: "N/A",
+      },
+    },
+
+    // for seller
+    seller: {
+      type: ObjectId,
+      ref: "User",
+    },
+
+    // for products
+    products: [
+      {
+        type: ObjectId,
+        ref: "Products",
+      },
+    ],
+
+    // for status
+    status: {
+      type: String,
+      enum: {
+        values: ["active", "inactive"],
+        message: "Invalid status, choose active/inactive",
+      },
+      default: "active",
+    },
+
+    // for keynotes
+    keynotes: [
+      {
+        type: String,
+        trim: true,
+      },
+    ],
+
+    // for tags
+    tags: [
+      {
+        type: String,
+        trim: true,
+      },
+    ],
+
+    // for trashable
+    trashable: {
+      type: Boolean,
+      default: false,
+    },
+
+    // for category  time stamps
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+    updatedAt: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  { timestamps: true }
+);
+
+/* middleware for store */
+storeSchema.pre("save", function (next) {
+  // replace space with hyphen and lowercase
+  const newTags = [];
+  this.tags.forEach((tag) =>
+    newTags.push(tag.replace(" ", "-")?.toLowerCase())
+  );
+  this.tags = newTags;
+
+  next();
+});
+
+/* create store schema model */
+const Store = mongoose.model("Store", storeSchema);
+
+/* export store schema */
+module.exports = Store;
