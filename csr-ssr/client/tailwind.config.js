edd5399739d7e1490cdd6f990c10087032d3a80f
devhasibulislam <devhasibulislam@gmail.com>
2023-11-14 22:52:35 +0600

    client dir init

---
+++ csr-ssr/client/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require("@tailwindcss/typography"),
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/aspect-ratio"),
+    require("tailwind-scrollbar-hide"),
+  ],
+};
