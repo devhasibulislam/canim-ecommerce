1a07bfbace1f44a2d5c7f3cdc106bb97accd0860
devhasibulislam <devhasibulislam@gmail.com>
2023-11-14 19:28:41 +0600

    ciseco api component init

---
+++ client/src/services/ciseco.js
@@ -0,0 +1,25 @@
+/**
+ * Title: Write a program using JavaScript on Ciseco
+ * Author: Hasibul Islam
+ * Portfolio: https://devhasibulislam.vercel.app
+ * Linkedin: https://linkedin.com/in/devhasibulislam
+ * GitHub: https://github.com/devhasibulislam
+ * Facebook: https://facebook.com/devhasibulislam
+ * Instagram: https://instagram.com/devhasibulislam
+ * Twitter: https://twitter.com/devhasibulislam
+ * Pinterest: https://pinterest.com/devhasibulislam
+ * WhatsApp: https://wa.me/8801906315901
+ * Telegram: devhasibulislam
+ * Date: 09, November 2023
+ */
+
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+export const cisecoApi = createApi({
+  reducerPath: "cisecoApi",
+  baseQuery: fetchBaseQuery({
+    baseUrl: `${process.env.NEXT_PUBLIC_BASE_URL}`,
+  }),
+  tagTypes: ["User", "Product", "Brand", "Category", "Store"],
+  endpoints: () => ({}),
+});
