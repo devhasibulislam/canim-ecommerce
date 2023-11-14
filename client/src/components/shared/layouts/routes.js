9106c48f2b4dd429acadb093ebbfe0fcd6603131
devhasibulislam <devhasibulislam@gmail.com>
2023-11-14 19:25:17 +0600

    dashboard sidebar items component init

---
+++ client/src/components/shared/layouts/routes.js
@@ -0,0 +1,69 @@
+const routes = [
+  {
+    name: "Product",
+    paths: [
+      {
+        name: "Add Product",
+        path: "/dashboard/add-product",
+      },
+      {
+        name: "List Products",
+        path: "/dashboard/list-products",
+      },
+    ],
+  },
+  {
+    name: "Category",
+    paths: [
+      {
+        name: "Add Category",
+        path: "/dashboard/add-category",
+      },
+      {
+        name: "List Categories",
+        path: "/dashboard/list-categories",
+      },
+    ],
+  },
+  {
+    name: "Store",
+    paths: [
+      {
+        name: "Add Store",
+        path: "/dashboard/add-store",
+      },
+      {
+        name: "List Stores",
+        path: "/dashboard/list-stores",
+      },
+    ],
+  },
+  {
+    name: "Brand",
+    paths: [
+      {
+        name: "Add Brand",
+        path: "/dashboard/add-brand",
+      },
+      {
+        name: "List Brands",
+        path: "/dashboard/list-brands",
+      },
+    ],
+  },
+  {
+    name: "User",
+    paths: [
+      {
+        name: "Add User",
+        path: "/dashboard/add-user",
+      },
+      {
+        name: "List Users",
+        path: "/dashboard/list-users",
+      },
+    ],
+  },
+];
+
+export default routes;
