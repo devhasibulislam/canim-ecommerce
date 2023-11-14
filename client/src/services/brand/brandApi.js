ccffcebc745c782c1d052798ec1fee68bb203b4f
devhasibulislam <devhasibulislam@gmail.com>
2023-11-14 19:27:50 +0600

    brand api component init

---
+++ client/src/services/brand/brandApi.js
@@ -0,0 +1,67 @@
+/**
+ * Title: Write a program using JavaScript on BrandApi
+ * Author: Hasibul Islam
+ * Portfolio: https://devhasibulislam.vercel.app
+ * Linkedin: https://linkedin.com/in/devhasibulislam
+ * GitHub: https://github.com/devhasibulislam
+ * Facebook: https://facebook.com/devhasibulislam
+ * Instagram: https://instagram.com/devhasibulislam
+ * Twitter: https://twitter.com/devhasibulislam
+ * Pinterest: https://pinterest.com/devhasibulislam
+ * WhatsApp: https://wa.me/8801906315901
+ * Telegram: devhasibulislam
+ * Date: 11, November 2023
+ */
+
+const { cisecoApi } = require("../ciseco");
+
+const brandApi = cisecoApi.injectEndpoints({
+  endpoints: (builder) => ({
+    // add new brand
+    addBrand: builder.mutation({
+      query: (brand) => ({
+        url: "/brand/add-brand",
+        method: "POST",
+        body: brand,
+      }),
+    }),
+
+    // get all brands
+    getBrands: builder.query({
+      query: () => ({
+        url: "/brand/list-brands",
+        method: "GET",
+      }),
+
+      providesTags: ["Brand"],
+    }),
+
+    // update brand
+    updateBrand: builder.mutation({
+      query: ({ id, body }) => ({
+        url: `/brand/update-brand/${id}`,
+        method: "PATCH",
+        body,
+      }),
+
+      invalidatesTags: ["Brand"],
+    }),
+
+    // get a brand
+    getBrand: builder.query({
+      query: (id) => ({
+        url: `/brand/get-brand/${id}`,
+        method: "GET",
+      }),
+
+      providesTags: ["Brand"],
+    }),
+  }),
+});
+
+export const {
+  useAddBrandMutation,
+  useGetBrandsQuery,
+  useUpdateBrandMutation,
+  useGetBrandQuery,
+} = brandApi;
