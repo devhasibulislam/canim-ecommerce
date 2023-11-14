1b409c37a8b669abf98a78a299cf0a3189bb99d7
devhasibulislam <devhasibulislam@gmail.com>
2023-11-14 19:46:47 +0600

    product controller init

---
+++ server/controllers/product.controller.js
@@ -0,0 +1,72 @@
+/**
+ * Title: Write a program using JavaScript on Product Controller
+ * Author: Hasibul Islam
+ * Portfolio: https://devhasibulislam.vercel.app
+ * Linkedin: https://linkedin.com/in/devhasibulislam
+ * GitHub: https://github.com/devhasibulislam
+ * Facebook: https://facebook.com/devhasibulislam
+ * Instagram: https:/instagram.com/devhasibulislam
+ * Twitter: https://twitter.com/devhasibulislam
+ * Pinterest: https://pinterest.com/devhasibulislam
+ * WhatsApp: https://wa.me/8801906315901
+ * Telegram: devhasibulislam
+ * Date: 11, November 2023
+ */
+
+/* internal import */
+const productService = require("../services/product.service");
+
+/* add new product */
+exports.addProduct = async (req, res, next) => {
+  try {
+    await productService.addProduct(req, res);
+  } catch (error) {
+    next(error);
+  } finally {
+    console.log(`Route: ${req.url} || Method: ${req.method}`);
+  }
+};
+
+/* get all products */
+exports.getProducts = async (req, res, next) => {
+  try {
+    await productService.getProducts(res);
+  } catch (error) {
+    next(error);
+  } finally {
+    console.log(`Route: ${req.url} || Method: ${req.method}`);
+  }
+};
+
+/* update product */
+exports.updateProduct = async (req, res, next) => {
+  try {
+    await productService.updateProduct(req, res);
+  } catch (error) {
+    next(error);
+  } finally {
+    console.log(`Route: ${req.url} || Method: ${req.method}`);
+  }
+};
+
+/* get a single product */
+exports.getProduct = async (req, res, next) => {
+  try {
+    await productService.getProduct(req, res);
+  } catch (error) {
+    next(error);
+  } finally {
+    console.log(`Route: ${req.url} || Method: ${req.method}`);
+  }
+};
+
+/* filtered products */
+exports.getFilteredProducts = async (req, res, next) => {
+  try {
+    await productService.getFilteredProducts(req, res);
+  } catch (error) {
+    next(error);
+  } finally {
+    console.log(`Route: ${req.url} || Method: ${req.method}`);
+  }
+};
