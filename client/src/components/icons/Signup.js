d31daa3e057654ef8b0a99a1f0aa346f2143de06
devhasibulislam <devhasibulislam@gmail.com>
2023-11-14 19:21:42 +0600

    sign up icon init

---
+++ client/src/components/icons/Signup.js
@@ -0,0 +1,36 @@
+/**
+ * Title: Write a program using JavaScript on Signup
+ * Author: Hasibul Islam
+ * Portfolio: https://devhasibulislam.vercel.app
+ * Linkedin: https://linkedin.com/in/devhasibulislam
+ * GitHub: https://github.com/devhasibulislam
+ * Facebook: https://facebook.com/devhasibulislam
+ * Instagram: https://instagram.com/devhasibulislam
+ * Twitter: https://twitter.com/devhasibulislam
+ * Pinterest: https://pinterest.com/devhasibulislam
+ * WhatsApp: https://wa.me/8801906315901
+ * Telegram: devhasibulislam
+ * Date: 08, November 2023
+ */
+
+import React from "react";
+
+const Signup = (props) => {
+  return (
+    <svg
+      {...props}
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      fill="currentColor"
+      className="w-6 h-6 text-sky-500"
+    >
+      <path
+        fillRule="evenodd"
+        d="M5.25 6.31v9.44a.75.75 0 01-1.5 0V4.5a.75.75 0 01.75-.75h11.25a.75.75 0 010 1.5H6.31l13.72 13.72a.75.75 0 11-1.06 1.06L5.25 6.31z"
+        clipRule="evenodd"
+      />
+    </svg>
+  );
+};
+
+export default Signup;
