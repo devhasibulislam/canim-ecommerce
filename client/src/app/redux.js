63214d12f9fbb29846477e6f370e1933bb0a567d
devhasibulislam <devhasibulislam@gmail.com>
2023-11-14 19:13:59 +0600

    setup redux toolkit on the project root

---
+++ client/src/app/redux.js
@@ -0,0 +1,30 @@
+/**
+ * Title: Write a program using JavaScript on Redux
+ * Author: Hasibul Islam
+ * Portfolio: https://devhasibulislam.vercel.app
+ * Linkedin: https://linkedin.com/in/devhasibulislam
+ * GitHub: https://github.com/devhasibulislam
+ * Facebook: https://facebook.com/devhasibulislam
+ * Instagram: https://instagram.com/devhasibulislam
+ * Twitter: https://twitter.com/devhasibulislam
+ * Pinterest: https://pinterest.com/devhasibulislam
+ * WhatsApp: https://wa.me/8801906315901
+ * Telegram: devhasibulislam
+ * Date: 09, November 2023
+ */
+
+"use client";
+
+import { Provider } from "react-redux";
+import { store } from "./store";
+import React from "react";
+
+const Redux = ({ children }) => {
+  return (
+    <Provider store={store}>
+      {children}
+    </Provider>
+  );
+};
+
+export default Redux;
