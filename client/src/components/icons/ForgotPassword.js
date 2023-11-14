8f092590db556f269778eb70cc11bc1205c3923e
devhasibulislam <devhasibulislam@gmail.com>
2023-11-14 19:20:04 +0600

    reset password icon init

---
+++ client/src/components/icons/ForgotPassword.js
@@ -0,0 +1,36 @@
+/**
+ * Title: Write a program using JavaScript on ForgotPassword
+ * Author: Hasibul Islam
+ * Portfolio: https://devhasibulislam.vercel.app
+ * Linkedin: https://linkedin.com/in/devhasibulislam
+ * GitHub: https://github.com/devhasibulislam
+ * Facebook: https://facebook.com/devhasibulislam
+ * Instagram: https://instagram.com/devhasibulislam
+ * Twitter: https://twitter.com/devhasibulislam
+ * Pinterest: https://pinterest.com/devhasibulislam
+ * WhatsApp: https://wa.me/8801906315901
+ * Telegram: devhasibulislam
+ * Date: 08, November 2023
+ */
+
+import React from "react";
+
+const ForgotPassword = (props) => {
+  return (
+    <svg
+      {...props}
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      fill="currentColor"
+      className="w-6 h-6 text-sky-500"
+    >
+      <path
+        fillRule="evenodd"
+        d="M15 3.75A5.25 5.25 0 009.75 9v10.19l4.72-4.72a.75.75 0 111.06 1.06l-6 6a.75.75 0 01-1.06 0l-6-6a.75.75 0 111.06-1.06l4.72 4.72V9a6.75 6.75 0 0113.5 0v3a.75.75 0 01-1.5 0V9c0-2.9-2.35-5.25-5.25-5.25z"
+        clipRule="evenodd"
+      />
+    </svg>
+  );
+};
+
+export default ForgotPassword;
