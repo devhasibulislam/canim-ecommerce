a16cbc3e09c161038f466373a371a18269f5d23c
devhasibulislam <devhasibulislam@gmail.com>
2023-11-14 19:31:03 +0600

    next config init

---
+++ client/next.config.js
@@ -0,0 +1,21 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "placehold.co",
+        port: "",
+        pathname: "**/*",
+      },
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+        port: "",
+        pathname: "**/*",
+      },
+    ],
+  },
+};
+
+module.exports = nextConfig;
