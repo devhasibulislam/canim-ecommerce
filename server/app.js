24cc30633fa02750f94ee80710bf1e3845739424
devhasibulislam <devhasibulislam@gmail.com>
2023-11-14 20:00:58 +0600

    server root init

---
+++ server/app.js
@@ -0,0 +1,64 @@
+/**
+ * Title: Write a program using JavaScript on App
+ * Author: Hasibul Islam
+ * Portfolio: https://devhasibulislam.vercel.app
+ * Linkedin: https://linkedin.com/in/devhasibulislam
+ * GitHub: https://github.com/devhasibulislam
+ * Facebook: https://facebook.com/devhasibulislam
+ * Instagram: https://instagram.com/devhasibulislam
+ * Twitter: https://twitter.com/devhasibulislam
+ * Pinterest: https://pinterest.com/devhasibulislam
+ * WhatsApp: https://wa.me/8801906315901
+ * Telegram: devhasibulislam
+ * Date: 09, November 2023
+ */
+
+/* external imports */
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+/* internal import */
+const error = require("./middleware/error.middleware");
+
+/* application level connection */
+const app = express();
+
+/* middleware connections */
+app.use(
+  cors({
+    origin: process.env.ORIGIN_URL,
+    methods: "GET, PATCH, POST, DELETE",
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+  })
+);
+app.use(express.json());
+
+/* router level connections */
+app.use("/api/brand", require("./routes/brand.route"));
+app.use("/api/category", require("./routes/category.route"));
+app.use("/api/product", require("./routes/product.route"));
+app.use("/api/store", require("./routes/store.route"));
+app.use("/api/user", require("./routes/user.route"));
+
+/* global error handler */
+app.use(error);
+
+/* connection establishment */
+app.get("/", (req, res, next) => {
+  try {
+    res.status(200).json({
+      acknowledgement: true,
+      message: "OK",
+      description: "The request is OK",
+    });
+  } catch (err) {
+    next(err);
+  } finally {
+    console.log(`Route: ${req.url} || Method: ${req.method}`);
+  }
+});
+
+/* export application */
+module.exports = app;
