6455a78ce91e9fd78b05b67bf5c48f32d6d46c73
devhasibulislam <devhasibulislam@gmail.com>
2023-11-14 19:10:37 +0600

    store list page init

---
+++ client/src/app/dashboard/list-stores/page.js
@@ -0,0 +1,90 @@
+/**
+ * Title: Write a program using JavaScript on Page
+ * Author: Hasibul Islam
+ * Portfolio: https://devhasibulislam.vercel.app
+ * Linkedin: https://linkedin.com/in/devhasibulislam
+ * GitHub: https://github.com/devhasibulislam
+ * Facebook: https://facebook.com/devhasibulislam
+ * Instagram: https://instagram.com/devhasibulislam
+ * Twitter: https://twitter.com/devhasibulislam
+ * Pinterest: https://pinterest.com/devhasibulislam
+ * WhatsApp: https://wa.me/8801906315901
+ * Telegram: devhasibulislam
+ * Date: 10, November 2023
+ */
+
+"use client";
+
+import DemoteStore from "@/components/dashboard/DemoteStore";
+import Pencil from "@/components/icons/Pencil";
+import Dashboard from "@/components/shared/layouts/Dashboard";
+import { useGetStoresQuery } from "@/services/store/storeApi";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+import React, { useEffect } from "react";
+
+const ListStores = () => {
+  const { data: storesData, isError: storesError } = useGetStoresQuery();
+  const stores = storesData?.data || [];
+  const router = useRouter();
+
+  useEffect(() => {
+    if (storesError) {
+      alert("Something went wrong, refresh the page.");
+    }
+  }, [storesError]);
+
+  return (
+    <Dashboard>
+      <div className="w-full grid grid-cols-3 gap-4">
+        {stores.map((store) => (
+          <div
+            key={store?._id}
+            className="flex flex-col gap-y-2 border p-4 rounded relative"
+          >
+            <Image
+              src={store?.thumbnail?.url}
+              alt={store?.thumbnail?.public_id}
+              height={30}
+              width={50}
+              className="rounded h-[30px] w-[50px] object-cover"
+            />
+            <article className="flex flex-col gap-y-1">
+              <h2 className="">{store?.title}</h2>
+              <p className="text-xs line-clamp-2 mb-1.5">
+                {store?.description}
+              </p>
+              <p className="text-xs flex flex-row flex-wrap gap-2">
+                {store?.tags.map((tag) => (
+                  <span
+                    key={tag}
+                    className="bg-purple-100/50 border border-purple-500 text-purple-500 px-1 rounded"
+                  >
+                    {`#${tag}`}
+                  </span>
+                ))}
+                <span className="!text-xs bg-indigo-100/50 border border-indigo-500 text-indigo-500 rounded w-fit px-2">
+                  {store?.products?.length} Products
+                </span>
+              </p>
+            </article>
+
+            <div className="absolute top-2 right-2 flex flex-row gap-x-2">
+              {!store?.trashable && <DemoteStore store={store} />}
+              <button
+                className="bg-green-50 border border-green-900 p-0.5 rounded-secondary text-green-900"
+                onClick={() =>
+                  router.push(`/dashboard/list-stores/${store?._id}`)
+                }
+              >
+                <Pencil />
+              </button>
+            </div>
+          </div>
+        ))}
+      </div>
+    </Dashboard>
+  );
+};
+
+export default ListStores;
