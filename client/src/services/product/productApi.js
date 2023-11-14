cdd695419989dc3c51b43e9be05a07aa43c124e9
devhasibulislam <devhasibulislam@gmail.com>
2023-11-14 19:28:09 +0600

    product api component init

---
+++ client/src/services/product/productApi.js
@@ -0,0 +1,79 @@
+/**
+ * Title: Write a program using JavaScript on ProductApi
+ * Author: Hasibul Islam
+ * Portfolio: https://devhasibulislam.vercel.app
+ * Linkedin: https://linkedin.com/in/devhasibulislam
+ * GitHub: https://github.com/devhasibulislam
+ * Facebook: https://facebook.com/devhasibulislam
+ * Instagram: https://instagram.com/devhasibulislam
+ * Twitter: https://twitter.com/devhasibulislam
+ * Pinterest: https://pinterest.com/devhasibulislam
+ * WhatsApp: https://wa.me/8801906315901
+ * Telegram: devhasibulislam
+ * Date: 11, November 2023
+ */
+
+const { cisecoApi } = require("../ciseco");
+
+const productApi = cisecoApi.injectEndpoints({
+  endpoints: (builder) => ({
+    // add new product
+    addProduct: builder.mutation({
+      query: (product) => ({
+        url: "/product/add-product",
+        method: "POST",
+        body: product,
+      }),
+    }),
+
+    // get all products
+    getProducts: builder.query({
+      query: () => ({
+        url: "/product/list-products",
+        method: "GET",
+      }),
+
+      providesTags: ["Product"],
+    }),
+
+    // update product
+    updateProduct: builder.mutation({
+      query: ({ id, body }) => ({
+        url: `/product/update-product/${id}`,
+        method: "PATCH",
+        body,
+      }),
+
+      invalidatesTags: ["Product"],
+    }),
+
+    // get a single product
+    getProduct: builder.query({
+      query: (id) => ({
+        url: `/product/get-product/${id}`,
+        method: "GET",
+      }),
+
+      providesTags: ["Product"],
+    }),
+
+    // filtered products
+    getFilteredProducts: builder.mutation({
+      query: (body) => ({
+        url: "/product/filtered-products",
+        method: "POST",
+        body,
+      }),
+
+      providesTags: ["Product"],
+    }),
+  }),
+});
+
+export const {
+  useAddProductMutation,
+  useGetProductsQuery,
+  useUpdateProductMutation,
+  useGetProductQuery,
+  useGetFilteredProductsMutation,
+} = productApi;
