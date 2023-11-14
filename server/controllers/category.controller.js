25bb80846c5342f716d8404ee33233956b7c834c
devhasibulislam <devhasibulislam@gmail.com>
2023-11-14 19:46:40 +0600

    category controller init

---
+++ server/controllers/category.controller.js
@@ -0,0 +1,61 @@
+/**
+ * Title: Write a program using JavaScript on Category Controller
+ * Author: Hasibul Islam
+ * Portfolio: https://devhasibulislam.vercel.app
+ * Linkedin: https://linkedin.com/in/devhasibulislam
+ * GitHub: https://github.com/devhasibulislam
+ * Facebook: https://facebook.com/devhasibulislam
+ * Instagram: https:/instagram.com/devhasibulislam
+ * Twitter: https://twitter.com/devhasibulislam
+ * Pinterest: https://pinterest.com/devhasibulislam
+ * WhatsApp: https://wa.me/8801906315901
+ * Telegram: devhasibulislam
+ * Date: 11, November 2023
+ */
+
+/* internal import */
+const categoryService = require("../services/category.service");
+
+/* add new category */
+exports.addCategory = async (req, res, next) => {
+  try {
+    await categoryService.addCategory(req, res);
+  } catch (error) {
+    next(error);
+  } finally {
+    console.log(`Route: ${req.url} || Method: ${req.method}`);
+  }
+};
+
+/* get all categories */
+exports.getCategories = async (req, res, next) => {
+  try {
+    await categoryService.getCategories(res);
+  } catch (error) {
+    next(error);
+  } finally {
+    console.log(`Route: ${req.url} || Method: ${req.method}`);
+  }
+};
+
+/* update category */
+exports.updateCategory = async (req, res, next) => {
+  try {
+    await categoryService.updateCategory(req, res);
+  } catch (error) {
+    next(error);
+  } finally {
+    console.log(`Route: ${req.url} || Method: ${req.method}`);
+  }
+};
+
+/* get a category */
+exports.getCategory = async (req, res, next) => {
+  try {
+    await categoryService.getCategory(req, res);
+  } catch (error) {
+    next(error);
+  } finally {
+    console.log(`Route: ${req.url} || Method: ${req.method}`);
+  }
+};
