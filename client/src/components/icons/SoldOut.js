1898ff470db4aec3b82de722caa6671d88f6eb2f
devhasibulislam <devhasibulislam@gmail.com>
2023-11-14 19:21:49 +0600

    sold out icon init

---
+++ client/src/components/icons/SoldOut.js
@@ -0,0 +1,39 @@
+/**
+ * Title: Write a program using JavaScript on SoldOut
+ * Author: Hasibul Islam
+ * Portfolio: https://devhasibulislam.vercel.app
+ * Linkedin: https://linkedin.com/in/devhasibulislam
+ * GitHub: https://github.com/devhasibulislam
+ * Facebook: https://facebook.com/devhasibulislam
+ * Instagram: https://instagram.com/devhasibulislam
+ * Twitter: https://twitter.com/devhasibulislam
+ * Pinterest: https://pinterest.com/devhasibulislam
+ * WhatsApp: https://wa.me/8801906315901
+ * Telegram: devhasibulislam
+ * Date: 15, October 2023
+ */
+
+import React from "react";
+
+const SoldOut = ({ className, ...props }) => {
+  return (
+    <svg
+      {...props}
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth="1.5"
+      stroke="currentColor"
+      aria-hidden="true"
+      className={"w-3.5 h-3.5" + (className ? " " + className : "")}
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M18.364 18.364A9 9 0 005.636 5.636m12.728 12.728A9 9 0 015.636 5.636m12.728 12.728L5.636 5.636"
+      ></path>
+    </svg>
+  );
+};
+
+export default SoldOut;
