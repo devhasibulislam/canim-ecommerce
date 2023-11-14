811f5d29b353e71e82b4fe04d566ed330f99cd70
devhasibulislam <devhasibulislam@gmail.com>
2023-11-14 19:46:52 +0600

    store controller init

---
+++ server/controllers/store.controller.js
@@ -0,0 +1,61 @@
+/**
+ * Title: Write a program using JavaScript on Store Controller
+ * Author: Hasibul Islam
+ * Portfolio: https://devhasibulislam.vercel.app
+ * Linkedin: https://linkedin.com/in/devhasibulislam
+ * GitHub: https://github.com/devhasibulislam
+ * Facebook: https://facebook.com/devhasibulislam
+ * Instagram: https:/instagram.com/devhasibulislam
+ * Twitter: https://twitter.com/devhasibulislam
+ * Pinterest: https://pinterest.com/devhasibulislam
+ * WhatsApp: https://wa.me/8801906315901
+ * Telegram: devhasibulislam
+ * Date: 11, November 2023
+ */
+
+/* internal imports */
+const storeService = require("../services/store.service");
+
+/* create store */
+exports.addStore = async (req, res, next) => {
+  try {
+    await storeService.addStore(req, res);
+  } catch (error) {
+    next(error);
+  } finally {
+    console.log(`Route: ${req.url} || Method: ${req.method}`);
+  }
+};
+
+/* get all stores */
+exports.getStores = async (req, res, next) => {
+  try {
+    await storeService.getStores(req, res);
+  } catch (error) {
+    next(error);
+  } finally {
+    console.log(`Route: ${req.url} || Method: ${req.method}`);
+  }
+};
+
+/* update store */
+exports.updateStore = async (req, res, next) => {
+  try {
+    await storeService.updateStore(req, res);
+  } catch (error) {
+    next(error);
+  } finally {
+    console.log(`Route: ${req.url} || Method: ${req.method}`);
+  }
+};
+
+// get a store
+exports.getStore = async (req, res, next) => {
+  try {
+    await storeService.getStore(req, res);
+  } catch (error) {
+    next(error);
+  } finally {
+    console.log(`Route: ${req.url} || Method: ${req.method}`);
+  }
+};
