816959aabc79bd9399b9e426db254dfaae515876
devhasibulislam <devhasibulislam@gmail.com>
2023-11-14 19:47:46 +0600

    category schema model init

---
+++ server/models/category.model.js
@@ -0,0 +1,119 @@
+/**
+ * Title: Write a program using JavaScript on Category Model
+ * Author: Hasibul Islam
+ * Portfolio: https://devhasibulislam.vercel.app
+ * Linkedin: https://linkedin.com/in/devhasibulislam
+ * GitHub: https://github.com/devhasibulislam
+ * Facebook: https://facebook.com/devhasibulislam
+ * Instagram: https:/instagram.com/devhasibulislam
+ * Twitter: https://twitter.com/devhasibulislam
+ * Pinterest: https://pinterest.com/devhasibulislam
+ * WhatsApp: https://wa.me/8801906315901
+ * Telegram: devhasibulislam
+ * Date: 11, November 2023
+ */
+
+/* external imports */
+const mongoose = require("mongoose");
+const validator = require("validator");
+
+/* create category schema */
+const categorySchema = new mongoose.Schema(
+  {
+    // for title
+    title: {
+      type: String,
+      required: [true, "Please, provide a category name"],
+      trim: true,
+      unique: [true, "Same category already exists"],
+    },
+
+    // for description
+    description: {
+      type: String,
+      required: [true, "Please, provide category description"],
+      trim: true,
+      maxLength: [500, "Category description would be at most 500 characters"],
+    },
+
+    // for thumbnail
+    thumbnail: {
+      url: {
+        type: String,
+        validate: [validator.isURL, "Please provide a valid thumbnail URL"],
+        default: "https://placehold.co/296x200.png",
+      },
+      public_id: {
+        type: String,
+        default: "N/A",
+      },
+    },
+
+    // for keynotes
+    keynotes: [
+      {
+        type: String,
+        trim: true,
+      },
+    ],
+
+    // for tags
+    tags: [
+      {
+        type: String,
+        trim: true,
+      },
+    ],
+
+    // for products
+    products: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Products",
+      },
+    ],
+
+    // for trashable
+    trashable: {
+      type: Boolean,
+      default: false,
+    },
+
+    // for category  time stamps
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+    updatedAt: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  { timestamps: true }
+);
+
+/* middleware for category */
+categorySchema.pre("save", function (next) {
+  // Capitalize title
+  let splitStr = this.title?.toLowerCase().split(" ");
+  for (let i = 0; i < splitStr.length; i++) {
+    splitStr[i] =
+      splitStr[i].charAt(0).toUpperCase() + splitStr[i].substring(1);
+  }
+  this.title = splitStr.join(" ");
+
+  // replace space with hyphen and lowercase
+  const newTags = [];
+  this.tags.forEach((tag) =>
+    newTags.push(tag.replace(" ", "-")?.toLowerCase())
+  );
+  this.tags = newTags;
+
+  next();
+});
+
+/* create category model schema */
+const Category = mongoose.model("Category", categorySchema);
+
+/* export category schema */
+module.exports = Category;
