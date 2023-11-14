6117ad603464393ae69178225b87c564494ef736
devhasibulislam <devhasibulislam@gmail.com>
2023-11-14 19:47:27 +0600

    verify user middleware init

---
+++ server/middleware/verify.middleware.js
@@ -0,0 +1,48 @@
+/**
+ * Title: Write a program using JavaScript on Verify Middleware
+ * Author: Hasibul Islam
+ * Portfolio: https://devhasibulislam.vercel.app
+ * Linkedin: https://linkedin.com/in/devhasibulislam
+ * GitHub: https://github.com/devhasibulislam
+ * Facebook: https://facebook.com/devhasibulislam
+ * Instagram: https:/instagram.com/devhasibulislam
+ * Twitter: https://twitter.com/devhasibulislam
+ * Pinterest: https://pinterest.com/devhasibulislam
+ * WhatsApp: https://wa.me/8801906315901
+ * Telegram: devhasibulislam
+ * Date: 12, November 2023
+ */
+
+/* external imports */
+const jwt = require("jsonwebtoken");
+const { promisify } = require("util");
+
+async function verify(req, res, next) {
+  try {
+    // catch the token from user header
+    const token = req.headers?.authorization?.split(" ")[1];
+
+    // no token explicitly give error
+    if (!token) {
+      return res.status(401).json({
+        acknowledgement: false,
+        message: "Unauthorized",
+        description: "No token found to persist an existing user for long time",
+      });
+    }
+
+    // fetching token set the user on request
+    const decoded = await promisify(jwt.verify)(
+      token,
+      process.env.TOKEN_SECRET
+    );
+    req.user = decoded;
+
+    next();
+  } catch (error) {
+    next(error);
+  }
+}
+
+/* export token verification */
+module.exports = verify;
