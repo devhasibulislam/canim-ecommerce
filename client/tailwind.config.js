852e90e6dd830ecf2f522a02e986034cfc3820f4
devhasibulislam <devhasibulislam@gmail.com>
2023-11-14 19:29:53 +0600

    tailwind css config init

---
+++ client/tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      borderRadius: {
+        DEFAULT: "10px",
+        primary: "1.5rem",
+        secondary: "9999px",
+      },
+    },
+  },
+  plugins: [require("@tailwindcss/forms"), require("tailwind-scrollbar-hide")],
+};
