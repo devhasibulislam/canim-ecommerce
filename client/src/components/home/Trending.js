c590fcb560697b6db162d01a7f12283a8f37db16
devhasibulislam <devhasibulislam@gmail.com>
2023-11-14 19:17:38 +0600

    trending component init

---
+++ client/src/components/home/Trending.js
@@ -0,0 +1,65 @@
+/**
+ * Title: Write a program using JavaScript on Trending
+ * Author: Hasibul Islam
+ * Portfolio: https://devhasibulislam.vercel.app
+ * Linkedin: https://linkedin.com/in/devhasibulislam
+ * GitHub: https://github.com/devhasibulislam
+ * Facebook: https://facebook.com/devhasibulislam
+ * Instagram: https://instagram.com/devhasibulislam
+ * Twitter: https://twitter.com/devhasibulislam
+ * Pinterest: https://pinterest.com/devhasibulislam
+ * WhatsApp: https://wa.me/8801906315901
+ * Telegram: devhasibulislam
+ * Date: 17, October 2023
+ */
+
+"use client";
+
+import React, { useEffect } from "react";
+import Container from "../shared/Container";
+import Card from "../shared/Card";
+import Spinner from "../shared/Spinner";
+import { useRouter } from "next/navigation";
+import { useGetProductsQuery } from "@/services/product/productApi";
+
+const Trending = () => {
+  const router = useRouter();
+  const { data: productsData, isError: productsError } = useGetProductsQuery();
+  const products = productsData?.data || [];
+
+  useEffect(() => {
+    if (productsError) {
+      alert("Something went wrong, refresh the page.");
+    }
+  }, [productsError]);
+
+  return (
+    <Container>
+      <section className="flex flex-col gap-y-10">
+        <div className="flex flex-col gap-y-1">
+          <h1 className="text-4xl">
+            What's <span className="">Trending Now</span>
+          </h1>
+          <p className="text-base">
+            Discover the most trending products in Canim.
+          </p>
+        </div>
+        <div className="flex flex-col gap-y-12">
+          <div className="grid lg:grid-cols-4 md:grid-cols-2 grid-cols-1 md:gap-x-6 gap-y-8">
+            {products?.slice(-16)?.map((product, index) => (
+              <Card key={index} product={product} />
+            ))}
+          </div>
+          <button
+            className="px-8 py-4 border border-black rounded-secondary bg-black hover:bg-black/90 text-white transition-colors drop-shadow w-fit mx-auto flex flex-row gap-x-2 items-center"
+            onClick={() => router.push("/products")}
+          >
+            <Spinner /> Show Me More
+          </button>
+        </div>
+      </section>
+    </Container>
+  );
+};
+
+export default Trending;
