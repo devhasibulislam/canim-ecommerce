456317478498ef39476b0820f6dae749bf569cab
devhasibulislam <devhasibulislam@gmail.com>
2023-11-14 19:46:58 +0600

    user controller init

---
+++ server/controllers/user.controller.js
@@ -0,0 +1,83 @@
+/**
+ * Title: Write a program using JavaScript on User Controller
+ * Author: Hasibul Islam
+ * Portfolio: https://devhasibulislam.vercel.app
+ * Linkedin: https://linkedin.com/in/devhasibulislam
+ * GitHub: https://github.com/devhasibulislam
+ * Facebook: https://facebook.com/devhasibulislam
+ * Instagram: https:/instagram.com/devhasibulislam
+ * Twitter: https://twitter.com/devhasibulislam
+ * Pinterest: https://pinterest.com/devhasibulislam
+ * WhatsApp: https://wa.me/8801906315901
+ * Telegram: devhasibulislam
+ * Date: 09, November 2023
+ */
+
+/* internal imports */
+const userService = require("../services/user.service");
+
+/* sign up an user */
+exports.signUp = async (req, res, next) => {
+  try {
+    await userService.signUp(req, res);
+  } catch (error) {
+    next(error);
+  } finally {
+    console.log(`Route: ${req.url} || Method: ${req.method}`);
+  }
+};
+
+/* sign in an user */
+exports.signIn = async (req, res, next) => {
+  try {
+    await userService.signIn(req, res);
+  } catch (error) {
+    next(error);
+  } finally {
+    console.log(`Route: ${req.url} || Method: ${req.method}`);
+  }
+};
+
+/* reset user password */
+exports.forgotPassword = async (req, res, next) => {
+  try {
+    await userService.forgotPassword(req, res);
+  } catch (error) {
+    next(error);
+  } finally {
+    console.log(`Route: ${req.url} || Method: ${req.method}`);
+  }
+};
+
+/* login persistance */
+exports.persistLogin = async (req, res, next) => {
+  try {
+    await userService.persistLogin(req, res);
+  } catch (error) {
+    next(error);
+  } finally {
+    console.log(`Route: ${req.url} || Method: ${req.method}`);
+  }
+};
+
+/* get all users */
+exports.getUsers = async (req, res, next) => {
+  try {
+    await userService.getUsers(res);
+  } catch (error) {
+    next(error);
+  } finally {
+    console.log(`Route: ${req.url} || Method: ${req.method}`);
+  }
+};
+
+/* update user */
+exports.updateUser = async (req, res, next) => {
+  try {
+    await userService.updateUser(req, res);
+  } catch (error) {
+    next(error);
+  } finally {
+    console.log(`Route: ${req.url} || Method: ${req.method}`);
+  }
+};
