342f40b9ad05c9d3f639a7702fd072810204fbae
devhasibulislam <devhasibulislam@gmail.com>
2023-11-14 19:47:10 +0600

    error handler middleware init

---
+++ server/middleware/error.middleware.js
@@ -0,0 +1,24 @@
+/**
+ * Title: Write a program using JavaScript on Error Middleware
+ * Author: Hasibul Islam
+ * Portfolio: https://devhasibulislam.vercel.app
+ * Linkedin: https://linkedin.com/in/devhasibulislam
+ * GitHub: https://github.com/devhasibulislam
+ * Facebook: https://facebook.com/devhasibulislam
+ * Instagram: https:/instagram.com/devhasibulislam
+ * Twitter: https://twitter.com/devhasibulislam
+ * Pinterest: https://pinterest.com/devhasibulislam
+ * WhatsApp: https://wa.me/8801906315901
+ * Telegram: devhasibulislam
+ * Date: 09, November 2023
+ */
+
+function error(err, _, res, _) {
+  res.send({
+    acknowledgement: false,
+    message: err.name,
+    description: err.message,
+  });
+}
+
+module.exports = error;
