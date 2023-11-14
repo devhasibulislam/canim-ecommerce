903a4c7a8df6d2df740b8838263c849568d79fa8
devhasibulislam <devhasibulislam@gmail.com>
2023-11-14 19:26:40 +0600

    spinner component init

---
+++ client/src/components/shared/Spinner.js
@@ -0,0 +1,44 @@
+/**
+ * Title: Write a program using JavaScript on Spinner
+ * Author: Hasibul Islam
+ * Portfolio: https://devhasibulislam.vercel.app
+ * Linkedin: https://linkedin.com/in/devhasibulislam
+ * GitHub: https://github.com/devhasibulislam
+ * Facebook: https://facebook.com/devhasibulislam
+ * Instagram: https://instagram.com/devhasibulislam
+ * Twitter: https://twitter.com/devhasibulislam
+ * Pinterest: https://pinterest.com/devhasibulislam
+ * WhatsApp: https://wa.me/8801906315901
+ * Telegram: devhasibulislam
+ * Date: 17, October 2023
+ */
+
+import React from "react";
+
+const Spinner = ({ className, ...props }) => {
+  return (
+    <svg
+      {...props}
+      className={"animate-spin h-5 w-5" + (className ? " " + className : "")}
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+    >
+      <circle
+        className="opacity-25"
+        cx="12"
+        cy="12"
+        r="10"
+        stroke="currentColor"
+        strokeWidth="3"
+      ></circle>
+      <path
+        className="opacity-75"
+        fill="currentColor"
+        d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+      ></path>
+    </svg>
+  );
+};
+
+export default Spinner;
