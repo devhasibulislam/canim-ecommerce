54cfc04c8aa550834782a746bea27e888637834e
devhasibulislam <devhasibulislam@gmail.com>
2023-11-14 19:46:31 +0600

    brand controller init

---
+++ server/controllers/brand.controller.js
@@ -0,0 +1,61 @@
+/**
+ * Title: Write a program using JavaScript on Brand Controller
+ * Author: Hasibul Islam
+ * Portfolio: https://devhasibulislam.vercel.app
+ * Linkedin: https://linkedin.com/in/devhasibulislam
+ * GitHub: https://github.com/devhasibulislam
+ * Facebook: https://facebook.com/devhasibulislam
+ * Instagram: https:/instagram.com/devhasibulislam
+ * Twitter: https://twitter.com/devhasibulislam
+ * Pinterest: https://pinterest.com/devhasibulislam
+ * WhatsApp: https://wa.me/8801906315901
+ * Telegram: devhasibulislam
+ * Date: 11, November 2023
+ */
+
+/* internal import */
+const brandService = require("../services/brand.service");
+
+/* add new brand */
+exports.addBrand = async (req, res, next) => {
+  try {
+    await brandService.addBrand(req, res);
+  } catch (error) {
+    next(error);
+  } finally {
+    console.log(`Route: ${req.url} || Method: ${req.method}`);
+  }
+};
+
+/* get all brands */
+exports.getBrands = async (req, res, next) => {
+  try {
+    await brandService.getBrands(res);
+  } catch (error) {
+    next(error);
+  } finally {
+    console.log(`Route: ${req.url} || Method: ${req.method}`);
+  }
+};
+
+/* update brand */
+exports.updateBrand = async (req, res, next) => {
+  try {
+    await brandService.updateBrand(req, res);
+  } catch (error) {
+    next(error);
+  } finally {
+    console.log(`Route: ${req.url} || Method: ${req.method}`);
+  }
+};
+
+/* get a brand */
+exports.getBrand = async (req, res, next) => {
+  try {
+    await brandService.getBrand(req, res);
+  } catch (error) {
+    next(error);
+  } finally {
+    console.log(`Route: ${req.url} || Method: ${req.method}`);
+  }
+};
