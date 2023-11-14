430e13fce9364ca8b3162abe0cafb2b90de44d8f
devhasibulislam <devhasibulislam@gmail.com>
2023-11-14 19:48:28 +0600

    category route init

---
+++ server/routes/category.route.js
@@ -0,0 +1,50 @@
+/**
+ * Title: Write a program using JavaScript on Category Route
+ * Author: Hasibul Islam
+ * Portfolio: https://devhasibulislam.vercel.app
+ * Linkedin: https://linkedin.com/in/devhasibulislam
+ * GitHub: https://github.com/devhasibulislam
+ * Facebook: https://facebook.com/devhasibulislam
+ * Instagram: https:/instagram.com/devhasibulislam
+ * Twitter: https://twitter.com/devhasibulislam
+ * Pinterest: https://pinterest.com/devhasibulislam
+ * WhatsApp: https://wa.me/8801906315901
+ * Telegram: devhasibulislam
+ * Date: 11, November 2023
+ */
+
+/* external import */
+const express = require("express");
+
+/* middleware imports */
+const upload = require("../middleware/upload.middleware");
+
+/* internal import */
+const categoryController = require("../controllers/category.controller");
+
+/* router level connection */
+const router = express.Router();
+
+/* router methods integration */
+
+// add new category
+router.post(
+  "/add-category",
+  upload.single("thumbnail"),
+  categoryController.addCategory
+);
+
+// get all categories
+router.get("/list-categories", categoryController.getCategories);
+
+// update category
+router.patch(
+  "/update-category/:id",
+  upload.single("thumbnail"),
+  categoryController.updateCategory
+);
+
+// get a category
+router.get("/get-category/:id", categoryController.getCategory);
+
+module.exports = router;
