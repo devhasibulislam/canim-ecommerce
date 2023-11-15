7f97e4db3552ef600e43f6790c1528c3bb09ff05
devhasibulislam <devhasibulislam@gmail.com>
2023-11-15 12:33:16 +0600

    modify the name to as previous

--- client/src/services/ciseco.js
+++ client/src/services/ciseco.js
@@ -18,7 +18,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const cisecoApi = createApi({
   reducerPath: "cisecoApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: `${process.env.API_ENDPOINT}`,
+    baseUrl: `${process.env.NEXT_PUBLIC_BASE_URL}`,
   }),
   tagTypes: ["User", "Product", "Brand", "Category", "Store"],
   endpoints: () => ({}),
