593002f49d86f34569e29b2207790c1d81cf3972
devhasibulislam <devhasibulislam@gmail.com>
2023-11-15 21:11:27 +0600

    optimize atlas db uri

--- server/index.js
+++ server/index.js
@@ -23,31 +23,13 @@ const consoleMessage = require("./utils/console.util");
 const port = process.env.PORT || 3000;
 
 /* database connection */
-const db = process.env.ATLAS_URI;
-
 mongoose
-  .connect(db, {
+  .connect(process.env.ATLAS_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
   .then(() => consoleMessage.successMessage("Connected to MongoDB."))
   .catch((error) => consoleMessage.errorMessage(error.message));
-
-// const db = "mongodb+srv://project-template-i:project-template-i@project-template-i.zcnsfab.mongodb.net/ciseco-template?retryWrites=true&w=majority";
-
-// const db = process.env.ATLAS_URI;
-
-// mongoose
-//   .connect(db, {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-//   })
-//   .then(() => {
-//     console.log("Connected to MongoDB");
-//   })
-//   .catch((err) => {
-//     console.log("Error: ", err.message);
-//   });
 
 /* establish server port */
 app.listen(port, () => {
