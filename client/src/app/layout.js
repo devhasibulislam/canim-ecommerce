f5d4d4448bd8bccf266dbcd5d5f1f6b16ac3d269
devhasibulislam <devhasibulislam@gmail.com>
2023-11-20 16:17:14 +0600

    meta data layout modify

--- client/src/app/layout.js
+++ client/src/app/layout.js
@@ -3,9 +3,22 @@ import "./globals.css";
 import Redux from "./redux";
 
 export const metadata = {
-  title: "Ciseco - Template",
+  title: "Ciseco - Shop & eCommerce React Template",
   description:
-    "An eco shop & e-commerce responsive React template built by React.JS & Tailwind CSS",
+    "Buy Ciseco - Shop & eCommerce Next.Js Template by Hasibul Islam. Ciseco | Shop & eCommerce React Template - a responsive React template. Ciseco is built with the latest Next.Js 13 App Directory",
+  keywords: ["Ciseco", "Shop", "Ecommerce", "React", "Template", "Next.js"],
+  openGraph: {
+    title: "Ciseco - Shop & eCommerce React Template",
+    description:
+      "Buy Ciseco - Shop & eCommerce Next.Js Template by Hasibul Islam. Ciseco | Shop & eCommerce React Template - a responsive React template. Ciseco is built with the latest Next.Js 13 App Directory",
+    keywords: ["Ciseco", "Shop", "Ecommerce", "React", "Template", "Next.js"],
+    url: "https://ciseco-csr.vercel.app/",
+    siteName: "Ciseco Template",
+    images:
+      "https://res.cloudinary.com/dho0rpn5a/image/upload/v1700475060/ciseco-template/ciseco-ecommerce_chsdds.png",
+    locale: "en_US",
+    type: "website",
+  },
 };
 
 export default function RootLayout({ children }) {
