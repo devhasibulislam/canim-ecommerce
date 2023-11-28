5e4ec896fe9433083e7079b1e290ea2d9a891829
Hasibul Islam <devhasibulislam@gmail.com>
2023-11-28 11:36:01 +0600

    modify card view of brands

--- client/src/app/dashboard/list-brands/page.js
+++ client/src/app/dashboard/list-brands/page.js
@@ -45,7 +45,7 @@ const ListBrands = () => {
         <DashboardLading />
       ) : (
         <>
-          <div className="w-full grid grid-cols-3 gap-4">
+          <div className="w-full grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-4">
             {brands.map((brand) => (
               <div
                 key={brand?._id}
