b0120d1233fc5ae863fb74b7785071c13c04b754
devhasibulislam <devhasibulislam@gmail.com>
2024-01-09 11:00:34 +0600

    meta information modify

--- client/src/app/layout.js
+++ client/src/app/layout.js
@@ -3,21 +3,29 @@ import "./globals.css";
 import Redux from "./redux";
 
 export const metadata = {
+  metadataBase: new URL("https://ciseco-csr.vercel.app"),
   title: "Ciseco - Shop & eCommerce React Template",
   description:
     "Buy Ciseco - Shop & eCommerce Next.Js Template by Hasibul Islam. Ciseco | Shop & eCommerce React Template - a responsive React template. Ciseco is built with the latest Next.Js 13 App Directory",
-  keywords: ["Ciseco", "Shop", "Ecommerce", "React", "Template", "Next.js"],
   openGraph: {
     title: "Ciseco - Shop & eCommerce React Template",
     description:
       "Buy Ciseco - Shop & eCommerce Next.Js Template by Hasibul Islam. Ciseco | Shop & eCommerce React Template - a responsive React template. Ciseco is built with the latest Next.Js 13 App Directory",
-    keywords: ["Ciseco", "Shop", "Ecommerce", "React", "Template", "Next.js"],
-    url: "https://ciseco-csr.vercel.app/",
+    url: "https://ciseco-csr.vercel.app",
     siteName: "Ciseco Template",
     images:
       "https://res.cloudinary.com/dho0rpn5a/image/upload/v1700475060/ciseco-template/ciseco-ecommerce_chsdds.png",
     locale: "en_US",
     type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    site: "@devhasibulislam",
+    title: "Ciseco - Shop & eCommerce React Template",
+    description:
+      "Buy Ciseco - Shop & eCommerce Next.Js Template by Hasibul Islam. Ciseco | Shop & eCommerce React Template - a responsive React template. Ciseco is built with the latest Next.Js 13 App Directory",
+    image:
+      "https://res.cloudinary.com/dho0rpn5a/image/upload/v1700475060/ciseco-template/ciseco-ecommerce_chsdds.png",
   },
 };
 
