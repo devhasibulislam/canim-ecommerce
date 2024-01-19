49564d9ae320e20b917f94497c2d82b7fa588719
devhasibulislam <devhasibulislam@gmail.com>
2024-01-19 22:06:13 +0600

    modify layout as root dir

--- client/src/app/layout.js
+++ client/src/app/layout.js
@@ -1,18 +1,19 @@
-import AppWrapper from "./AppWrapper";
+import Auth from "./auth";
+import Providers from "./providers";
 import "./globals.css";
-import Redux from "./redux";
+import { Toaster } from "react-hot-toast";
 
 export const metadata = {
-  metadataBase: new URL("https://ciseco-csr.vercel.app"),
-  title: "Ciseco - Shop & eCommerce React Template",
+  metadataBase: new URL("https://canim-csr.vercel.app"),
+  title: "Canim - Shop & eCommerce React Template",
   description:
-    "Buy Ciseco - Shop & eCommerce Next.Js Template by Hasibul Islam. Ciseco | Shop & eCommerce React Template - a responsive React template. Ciseco is built with the latest Next.Js 13 App Directory",
+    "Buy Canim - Shop & eCommerce Next.Js Template by Hasibul Islam. Canim | Shop & eCommerce React Template - a responsive React template. Canim is built with the latest Next.Js 13 App Directory",
   openGraph: {
-    title: "Ciseco - Shop & eCommerce React Template",
+    title: "Canim - Shop & eCommerce React Template",
     description:
-      "Buy Ciseco - Shop & eCommerce Next.Js Template by Hasibul Islam. Ciseco | Shop & eCommerce React Template - a responsive React template. Ciseco is built with the latest Next.Js 13 App Directory",
-    url: "https://ciseco-csr.vercel.app",
-    siteName: "Ciseco Template",
+      "Buy Canim - Shop & eCommerce Next.Js Template by Hasibul Islam. Canim | Shop & eCommerce React Template - a responsive React template. Canim is built with the latest Next.Js 13 App Directory",
+    url: "https://canim-csr.vercel.app",
+    siteName: "Canim Template",
     images:
       "https://res.cloudinary.com/dho0rpn5a/image/upload/v1700475060/ciseco-template/ciseco-ecommerce_chsdds.png",
     locale: "en_US",
@@ -21,9 +22,9 @@ export const metadata = {
   twitter: {
     card: "summary_large_image",
     site: "@devhasibulislam",
-    title: "Ciseco - Shop & eCommerce React Template",
+    title: "Canim - Shop & eCommerce React Template",
     description:
-      "Buy Ciseco - Shop & eCommerce Next.Js Template by Hasibul Islam. Ciseco | Shop & eCommerce React Template - a responsive React template. Ciseco is built with the latest Next.Js 13 App Directory",
+      "Buy Canim - Shop & eCommerce Next.Js Template by Hasibul Islam. Canim | Shop & eCommerce React Template - a responsive React template. Canim is built with the latest Next.Js 13 App Directory",
     image:
       "https://res.cloudinary.com/dho0rpn5a/image/upload/v1700475060/ciseco-template/ciseco-ecommerce_chsdds.png",
   },
@@ -33,9 +34,10 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
-        <Redux>
-          <AppWrapper>{children}</AppWrapper>
-        </Redux>
+        <Providers>
+          <Auth>{children}</Auth>
+          <Toaster />
+        </Providers>
       </body>
     </html>
   );
