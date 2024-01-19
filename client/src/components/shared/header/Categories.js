dffe0f401d1104ca641429f4f0817faf5a5c400b
devhasibulislam <devhasibulislam@gmail.com>
2024-01-19 22:10:24 +0600

    categories component init

--- client/src/components/shared/header/Categories.js
+++ client/src/components/shared/header/Categories.js
@@ -15,34 +15,68 @@
 
 "use client";
 
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useMemo, useState } from "react";
 import OutsideClick from "../OutsideClick";
 import { BiCategory, BiChevronDown } from "react-icons/bi";
 import { useGetCategoriesQuery } from "@/services/category/categoryApi";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
 import CategoryCard from "../skeletonLoading/CategoryCard";
+import { toast } from "react-hot-toast";
+import { useGetBrandsQuery } from "@/services/brand/brandApi";
+import { useGetStoresQuery } from "@/services/store/storeApi";
 
 const Categories = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const [tab, setTab] = useState("categories");
   const {
     data: categoriesData,
     error: categoriesError,
     isLoading: categoriesLoading,
   } = useGetCategoriesQuery();
-  const categories = categoriesData?.data || [];
+  const categories = useMemo(
+    () => categoriesData?.data || [],
+    [categoriesData]
+  );
+  const {
+    isLoading: brandsLoading,
+    error: brandsError,
+    data: brandsData,
+  } = useGetBrandsQuery();
+  const brands = useMemo(() => brandsData?.data || [], [brandsData]);
+  const {
+    isLoading: storesLoading,
+    data: storesData,
+    error: storesError,
+  } = useGetStoresQuery();
+  const stores = useMemo(() => storesData?.data || [], [storesData]);
+
   const router = useRouter();
 
   useEffect(() => {
     if (categoriesError) {
-      alert(categoriesError?.data?.description);
+      toast.error(categoriesError?.data?.description, {
+        id: "categoriesData",
+      });
     }
-  }, [categoriesError]);
+
+    if (brandsError) {
+      toast.error(brandsError?.data?.description, {
+        id: "brandsData",
+      });
+    }
+
+    if (storesError) {
+      toast.error(storesError?.data?.description, {
+        id: "storesData",
+      });
+    }
+  }, [categoriesError, brandsError, storesError]);
 
   return (
     <>
       <button
-        className="border px-2.5 py-1.5 rounded-primary flex flex-row items-center gap-x-0.5 hover:border-black transition-colors"
+        className="border px-2.5 py-1.5 rounded flex flex-row items-center gap-x-0.5 hover:border-black transition-colors"
         onClick={() => setIsOpen(!isOpen)}
       >
         <BiCategory className="h-6 w-6" />
@@ -52,48 +86,165 @@ const Categories = () => {
       {isOpen && (
         <OutsideClick
           onOutsideClick={() => setIsOpen(false)}
-          className="absolute top-full left-0 w-80 max-h-96 overflow-y-auto bg-white border rounded p-4 flex flex-col gap-y-2.5"
+          className="absolute top-full left-0 w-80 h-96 overflow-y-auto bg-white border rounded p-4 flex flex-col gap-y-4"
         >
-          <>
-            {categoriesLoading ? (
-              <>
-                {[1, 2, 3, 4].map((_, index) => (
-                  <CategoryCard key={index} />
-                ))}
-              </>
-            ) : (
-              <>
-                {categories.map((category) => (
-                  <div
-                    key={category?._id}
-                    className="w-full flex flex-row items-start gap-x-2 p-2 border border-transparent hover:border-black rounded cursor-pointer"
-                    onClick={() => {
-                      router.push("/products/");
-                      setIsOpen(false);
-                    }}
-                  >
-                    <Image
-                      src={category?.thumbnail?.url}
-                      alt={category?.thumbnail?.public_id}
-                      width={40}
-                      height={40}
-                      className="h-[40px] w-[40px] object-cover rounded"
-                    />
-                    <article className="whitespace-normal">
-                      <h2 className="text-sm">{category?.title}</h2>
-                      <p className="text-xs line-clamp-2">
-                        {category?.description}
-                      </p>
-                      <span className="text-[10px] bg-purple-300/50 text-purple-500 border border-purple-500 px-1.5 rounded">
-                        Products: {category?.products?.length}
-                      </span>
-                    </article>
-                  </div>
-                ))}
-              </>
-            )}
-          </>
-          {categories?.length === 0 && <>No Categories Found!</>}
+          <section className="flex flex-col gap-y-4 h-full">
+            <div className="flex flex-row gap-x-2">
+              <button
+                type="button"
+                className={`text-xs px-2 py-1 border rounded ${
+                  tab === "categories" ? "!bg-black !text-white" : ""
+                }`}
+                onClick={() => setTab("categories")}
+              >
+                Categories
+              </button>
+              <button
+                type="button"
+                className={`text-xs px-2 py-1 border rounded ${
+                  tab === "brands" ? "!bg-black !text-white" : ""
+                }`}
+                onClick={() => setTab("brands")}
+              >
+                Brands
+              </button>
+              <button
+                type="button"
+                className={`text-xs px-2 py-1 border rounded ${
+                  tab === "stores" ? "!bg-black !text-white" : ""
+                }`}
+                onClick={() => setTab("stores")}
+              >
+                Stores
+              </button>
+            </div>
+
+            <div className="h-full overflow-y-auto scrollbar-hide">
+              {tab === "categories" && (
+                <>
+                  {categoriesLoading || categories?.length === 0 ? (
+                    <>
+                      {[1, 2, 3, 4].map((_, index) => (
+                        <CategoryCard key={index} />
+                      ))}
+                    </>
+                  ) : (
+                    <>
+                      {categories.map((category) => (
+                        <div
+                          key={category?._id}
+                          className="w-full flex flex-row items-start gap-x-2 p-2 border border-transparent hover:border-black rounded cursor-pointer"
+                          onClick={() => {
+                            router.push("/products/");
+                            setIsOpen(false);
+                          }}
+                        >
+                          <Image
+                            src={category?.thumbnail?.url}
+                            alt={category?.thumbnail?.public_id}
+                            width={40}
+                            height={40}
+                            className="h-[40px] w-[40px] object-cover rounded"
+                          />
+                          <article className="whitespace-normal">
+                            <h2 className="text-sm">{category?.title}</h2>
+                            <p className="text-xs line-clamp-2">
+                              {category?.description}
+                            </p>
+                            <span className="text-[10px] bg-purple-300/50 text-purple-500 border border-purple-500 px-1.5 rounded">
+                              Products: {category?.products?.length}
+                            </span>
+                          </article>
+                        </div>
+                      ))}
+                    </>
+                  )}
+                </>
+              )}
+              {tab === "brands" && (
+                <>
+                  {brandsLoading || brands?.length === 0 ? (
+                    <>
+                      {[1, 2, 3, 4].map((_, index) => (
+                        <CategoryCard key={index} />
+                      ))}
+                    </>
+                  ) : (
+                    <>
+                      {brands.map((brand) => (
+                        <div
+                          key={brand?._id}
+                          className="w-full flex flex-row items-start gap-x-2 p-2 border border-transparent hover:border-black rounded cursor-pointer"
+                          onClick={() => {
+                            router.push("/products/");
+                            setIsOpen(false);
+                          }}
+                        >
+                          <Image
+                            src={brand?.logo?.url}
+                            alt={brand?.logo?.public_id}
+                            width={40}
+                            height={40}
+                            className="h-[40px] w-[40px] object-cover rounded"
+                          />
+                          <article className="whitespace-normal">
+                            <h2 className="text-sm">{brand?.title}</h2>
+                            <p className="text-xs line-clamp-2">
+                              {brand?.description}
+                            </p>
+                            <span className="text-[10px] bg-purple-300/50 text-purple-500 border border-purple-500 px-1.5 rounded">
+                              Products: {brand?.products?.length}
+                            </span>
+                          </article>
+                        </div>
+                      ))}
+                    </>
+                  )}
+                </>
+              )}
+              {tab === "stores" && (
+                <>
+                  {storesLoading || stores?.length === 0 ? (
+                    <>
+                      {[1, 2, 3, 4].map((_, index) => (
+                        <CategoryCard key={index} />
+                      ))}
+                    </>
+                  ) : (
+                    <>
+                      {stores.map((store) => (
+                        <div
+                          key={store?._id}
+                          className="w-full flex flex-row items-start gap-x-2 p-2 border border-transparent hover:border-black rounded cursor-pointer"
+                          onClick={() => {
+                            router.push("/products/");
+                            setIsOpen(false);
+                          }}
+                        >
+                          <Image
+                            src={store?.thumbnail?.url}
+                            alt={store?.thumbnail?.public_id}
+                            width={40}
+                            height={40}
+                            className="h-[40px] w-[40px] object-cover rounded"
+                          />
+                          <article className="whitespace-normal">
+                            <h2 className="text-sm">{store?.title}</h2>
+                            <p className="text-xs line-clamp-2">
+                              {store?.description}
+                            </p>
+                            <span className="text-[10px] bg-purple-300/50 text-purple-500 border border-purple-500 px-1.5 rounded">
+                              Products: {store?.products?.length}
+                            </span>
+                          </article>
+                        </div>
+                      ))}
+                    </>
+                  )}
+                </>
+              )}
+            </div>
+          </section>
         </OutsideClick>
       )}
     </>
