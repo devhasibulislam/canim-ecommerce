f4c5a1d5ea9f84caece359fc8e1506f42e17f401
devhasibulislam <devhasibulislam@gmail.com>
2024-01-19 22:03:33 +0600

    niche explorer component init

--- client/src/components/home/NicheExplorer.js
+++ client/src/components/home/NicheExplorer.js
@@ -15,7 +15,7 @@
 
 "use client";
 
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useMemo, useState } from "react";
 import Container from "../shared/Container";
 import Brand from "../icons/Brand";
 import Category from "../icons/Category";
@@ -27,6 +27,7 @@ import { useGetStoresQuery } from "@/services/store/storeApi";
 import { useGetBrandsQuery } from "@/services/brand/brandApi";
 import { useGetCategoriesQuery } from "@/services/category/categoryApi";
 import Niche from "../shared/skeletonLoading/Niche";
+import { toast } from "react-hot-toast";
 
 const NicheExplorer = () => {
   const niches = [
@@ -45,36 +46,6 @@ const NicheExplorer = () => {
   ];
 
   const [selectedNiche, setSelectedNiche] = useState("Category");
-
-  const {
-    data: brandsData,
-    error: brandsError,
-    isLoading: fetchingBrands,
-  } = useGetBrandsQuery();
-  const {
-    data: categoriesData,
-    error: categoriesError,
-    isLoading: fetchingCategories,
-  } = useGetCategoriesQuery();
-  const {
-    data: storesData,
-    error: storesError,
-    isLoading: fetchingStores,
-  } = useGetStoresQuery();
-
-  const brands = brandsData?.data || [];
-  const categories = categoriesData?.data || [];
-  const stores = storesData?.data || [];
-
-  useEffect(() => {
-    if (brandsError || categoriesError || storesError) {
-      alert(
-        brandsError?.data?.description ||
-          categoriesError?.data?.description ||
-          storesError?.data?.description
-      );
-    }
-  }, [brandsError, categoriesError, storesError]);
 
   return (
     <Container>
@@ -102,152 +73,220 @@ const NicheExplorer = () => {
               ))}
             </div>
           </div>
-          <div className="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-4">
-            {selectedNiche === "Brand" &&
-              (fetchingBrands || brands?.length === 0 ? (
-                <>
-                  {[1, 2, 3, 4].map((_, index) => (
-                    <Niche key={index} />
-                  ))}
-                </>
-              ) : (
-                <>
-                  {brands?.slice(0, 6)?.map((brand, index) => (
-                    <div
-                      key={index}
-                      className="group border p-4 rounded-primary flex flex-col gap-y-4 hover:border-black transition-colors bg-white relative"
-                    >
-                      <Image
-                        src={brand?.logo?.url}
-                        alt={brand?.logo?.public_id}
-                        width={50}
-                        height={50}
-                        className="rounded h-[50px] w-[50px] object-cover"
-                      />
-
-                      <div className="flex flex-col gap-y-2">
-                        <h2 className="text-xl">{brand?.title}</h2>
-                        <p className="flex flex-row gap-x-1 items-center rounded-primary">
-                          <BsBoxSeam />{" "}
-                          <span className="group-hover:text-indigo-500 text-xs">
-                            {brand?.products?.length} Products
-                          </span>
-                        </p>
-                      </div>
-
-                      <p className="flex flex-row gap-1 overflow-x-auto scrollbar-hide">
-                        {brand.tags.map((tag, index) => (
-                          <span
-                            key={index}
-                            className="border text-xs px-1 py-0.5 rounded whitespace-nowrap"
-                          >
-                            {tag}
-                          </span>
-                        ))}
-                      </p>
-                    </div>
-                  ))}
-                </>
-              ))}
-
-            {selectedNiche === "Category" &&
-              (fetchingCategories || categories?.length === 0 ? (
-                <>
-                  {[1, 2, 3, 4].map((_, index) => (
-                    <Niche key={index} />
-                  ))}
-                </>
-              ) : (
-                <>
-                  {categories?.slice(0, 6)?.map((category, index) => (
-                    <div
-                      key={index}
-                      className="group border p-4 rounded-primary flex flex-col gap-y-4 hover:border-black transition-colors bg-white relative"
-                    >
-                      <Image
-                        src={category?.thumbnail?.url}
-                        alt={category?.thumbnail?.public_id}
-                        width={50}
-                        height={50}
-                        className="rounded h-[50px] w-[50px] object-cover"
-                      />
-
-                      <div className="flex flex-col gap-y-2">
-                        <h2 className="text-xl">{category?.title}</h2>
-                        <p className="flex flex-row gap-x-1 items-center rounded-primary">
-                          <BsBoxSeam />{" "}
-                          <span className="group-hover:text-indigo-500 text-xs transition-colors">
-                            {category?.products?.length} Products
-                          </span>
-                        </p>
-                      </div>
-
-                      <p className="flex flex-row gap-1 overflow-x-auto scrollbar-hide">
-                        {category.tags.map((tag, index) => (
-                          <span
-                            key={index}
-                            className="border text-xs px-1 py-0.5 rounded whitespace-nowrap"
-                          >
-                            {tag}
-                          </span>
-                        ))}
-                      </p>
-                    </div>
-                  ))}
-                </>
-              ))}
-
-            {selectedNiche === "Store" &&
-              (fetchingStores || stores?.length === 0 ? (
-                <>
-                  {[1, 2, 3, 4].map((_, index) => (
-                    <Niche key={index} />
-                  ))}
-                </>
-              ) : (
-                <>
-                  {stores?.slice(0, 6)?.map((store, index) => (
-                    <div
-                      key={index}
-                      className="group border p-4 rounded-primary flex flex-col gap-y-4 hover:border-black transition-colors bg-white relative"
-                    >
-                      <Image
-                        src={store?.thumbnail?.url}
-                        alt={store?.thumbnail?.public_id}
-                        width={50}
-                        height={50}
-                        className="rounded h-[50px] w-[50px] object-cover"
-                      />
-
-                      <div className="flex flex-col gap-y-2">
-                        <h2 className="text-xl">{store?.title}</h2>
-                        <p className="flex flex-row gap-x-1 items-center rounded-primary">
-                          <BsBoxSeam />{" "}
-                          <span className="group-hover:text-indigo-500 text-xs transition-colors">
-                            {store?.products?.length} Products
-                          </span>
-                        </p>
-                      </div>
-
-                      <p className="flex flex-row gap-1 overflow-x-auto scrollbar-hide">
-                        {store.tags.map((tag, index) => (
-                          <span
-                            key={index}
-                            className="border text-xs px-1 py-0.5 rounded whitespace-nowrap"
-                          >
-                            {tag}
-                          </span>
-                        ))}
-                      </p>
-                    </div>
-                  ))}
-                </>
-              ))}
-          </div>
+          {selectedNiche === "Brand" && <DisplayBrands />}
+          {selectedNiche === "Category" && <DisplayCategories />}
+          {selectedNiche === "Store" && <DisplayStores />}
         </div>
       </section>
     </Container>
   );
 };
+
+function DisplayBrands() {
+  const {
+    data: brandsData,
+    error: brandsError,
+    isLoading: fetchingBrands,
+  } = useGetBrandsQuery();
+
+  const brands = useMemo(() => brandsData?.data || [], [brandsData]);
+
+  useEffect(() => {
+    if (brandsError) {
+      toast.error(brandsError?.data?.description, {
+        id: "brands-data",
+      });
+    }
+  }, [brandsError]);
+
+  return (
+    <div className="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-4">
+      {fetchingBrands || brands?.length === 0 ? (
+        <>
+          {[1, 2, 3].map((_, index) => (
+            <Niche key={index} />
+          ))}
+        </>
+      ) : (
+        <>
+          {brands?.slice(0, 6)?.map((brand, index) => (
+            <div
+              key={index}
+              className="group border p-4 rounded-lg flex flex-col gap-y-4 hover:border-black transition-colors bg-white relative"
+            >
+              <Image
+                src={brand?.logo?.url}
+                alt={brand?.logo?.public_id}
+                width={50}
+                height={50}
+                className="rounded h-[50px] w-[50px] object-cover"
+              />
+
+              <div className="flex flex-col gap-y-2">
+                <h2 className="text-xl">{brand?.title}</h2>
+                <p className="flex flex-row gap-x-1 items-center rounded-primary">
+                  <BsBoxSeam />{" "}
+                  <span className="group-hover:text-indigo-500 text-xs">
+                    {brand?.products?.length} Products
+                  </span>
+                </p>
+              </div>
+
+              <p className="flex flex-row gap-1 overflow-x-auto scrollbar-hide">
+                {brand.tags.map((tag, index) => (
+                  <span
+                    key={index}
+                    className="border text-xs px-1 py-0.5 rounded whitespace-nowrap"
+                  >
+                    {tag}
+                  </span>
+                ))}
+              </p>
+            </div>
+          ))}
+        </>
+      )}
+    </div>
+  );
+}
+
+function DisplayCategories() {
+  const {
+    data: categoriesData,
+    error: categoriesError,
+    isLoading: fetchingCategories,
+  } = useGetCategoriesQuery();
+
+  const categories = useMemo(
+    () => categoriesData?.data || [],
+    [categoriesData]
+  );
+
+  useEffect(() => {
+    if (categoriesError) {
+      toast.error(categoriesError?.data?.description, {
+        id: "categories-data",
+      });
+    }
+  }, [categoriesError]);
+
+  return (
+    <div className="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-4">
+      {fetchingCategories || categories?.length === 0 ? (
+        <>
+          {[1, 2, 3].map((_, index) => (
+            <Niche key={index} />
+          ))}
+        </>
+      ) : (
+        <>
+          {categories?.slice(0, 6)?.map((category, index) => (
+            <div
+              key={index}
+              className="group border p-4 rounded-lg flex flex-col gap-y-4 hover:border-black transition-colors bg-white relative"
+            >
+              <Image
+                src={category?.thumbnail?.url}
+                alt={category?.thumbnail?.public_id}
+                width={50}
+                height={50}
+                className="rounded h-[50px] w-[50px] object-cover"
+              />
+
+              <div className="flex flex-col gap-y-2">
+                <h2 className="text-xl">{category?.title}</h2>
+                <p className="flex flex-row gap-x-1 items-center rounded-primary">
+                  <BsBoxSeam />{" "}
+                  <span className="group-hover:text-indigo-500 text-xs transition-colors">
+                    {category?.products?.length} Products
+                  </span>
+                </p>
+              </div>
+
+              <p className="flex flex-row gap-1 overflow-x-auto scrollbar-hide">
+                {category.tags.map((tag, index) => (
+                  <span
+                    key={index}
+                    className="border text-xs px-1 py-0.5 rounded whitespace-nowrap"
+                  >
+                    {tag}
+                  </span>
+                ))}
+              </p>
+            </div>
+          ))}
+        </>
+      )}
+    </div>
+  );
+}
+
+function DisplayStores() {
+  const {
+    data: storesData,
+    error: storesError,
+    isLoading: fetchingStores,
+  } = useGetStoresQuery();
+
+  const stores = useMemo(() => storesData?.data || [], [storesData]);
+
+  useEffect(() => {
+    if (storesError) {
+      toast.error(storesError?.data?.description, {
+        id: "stores-data",
+      });
+    }
+  }, [storesError]);
+
+  return (
+    <div className="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-4">
+      {fetchingStores || stores?.length === 0 ? (
+        <>
+          {[1, 2, 3].map((_, index) => (
+            <Niche key={index} />
+          ))}
+        </>
+      ) : (
+        <>
+          {stores?.slice(0, 6)?.map((store, index) => (
+            <div
+              key={index}
+              className="group border p-4 rounded-lg flex flex-col gap-y-4 hover:border-black transition-colors bg-white relative"
+            >
+              <Image
+                src={store?.thumbnail?.url}
+                alt={store?.thumbnail?.public_id}
+                width={50}
+                height={50}
+                className="rounded h-[50px] w-[50px] object-cover"
+              />
+
+              <div className="flex flex-col gap-y-2">
+                <h2 className="text-xl">{store?.title}</h2>
+                <p className="flex flex-row gap-x-1 items-center rounded-primary">
+                  <BsBoxSeam />{" "}
+                  <span className="group-hover:text-indigo-500 text-xs transition-colors">
+                    {store?.products?.length} Products
+                  </span>
+                </p>
+              </div>
+
+              <p className="flex flex-row gap-1 overflow-x-auto scrollbar-hide">
+                {store.tags.map((tag, index) => (
+                  <span
+                    key={index}
+                    className="border text-xs px-1 py-0.5 rounded whitespace-nowrap"
+                  >
+                    {tag}
+                  </span>
+                ))}
+              </p>
+            </div>
+          ))}
+        </>
+      )}
+    </div>
+  );
+}
 
 export default NicheExplorer;
