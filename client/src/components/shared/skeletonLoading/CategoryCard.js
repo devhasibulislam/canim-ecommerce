deab946e21d2c8b3b89b96cd891b7ce15f7e64c4
devhasibulislam <devhasibulislam@gmail.com>
2024-01-19 22:04:22 +0600

    modify category card component

--- client/src/components/shared/skeletonLoading/CategoryCard.js
+++ client/src/components/shared/skeletonLoading/CategoryCard.js
@@ -17,11 +17,12 @@ import React from "react";
 
 const CategoryCard = () => {
   return (
-    <section className="flex flex-row gap-x-2.5 w-full">
-      <div className="h-8 w-8 rounded-secondary bg-gray-200 animate-pulse" />
+    <section className="flex flex-row items-start gap-x-2.5 w-full">
+      <div className="!h-8 !w-10 rounded bg-gray-200 animate-pulse" />
       <div className="flex flex-col gap-y-1 w-full">
-        <div className="flex-1 h-4 rounded-secondary bg-gray-200 animate-pulse" />
-        <div className="flex-1 h-2 w-1/2 rounded-secondary bg-gray-200 animate-pulse" />
+        <div className="h-4 w-full rounded-secondary bg-gray-200 animate-pulse" />
+        <div className="h-3 w-3/4 rounded-secondary bg-gray-200 animate-pulse" />
+        <div className="h-2 w-1/2 rounded-secondary bg-gray-200 animate-pulse" />
       </div>
     </section>
   );
