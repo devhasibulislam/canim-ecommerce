89599c8e1b002eb30e17577d8fef02a1550ed451
devhasibulislam <devhasibulislam@gmail.com>
2024-01-20 11:09:16 +0600

    integrate product slice

---
+++ client/src/features/product/productSlice.js
@@ -0,0 +1,37 @@
+/**
+ * Title: Write a program using JavaScript on ProductSlice
+ * Author: Hasibul Islam
+ * Portfolio: https://devhasibulislam.vercel.app
+ * Linkedin: https://linkedin.com/in/devhasibulislam
+ * GitHub: https://github.com/devhasibulislam
+ * Facebook: https://facebook.com/devhasibulislam
+ * Instagram: https://instagram.com/devhasibulislam
+ * Twitter: https://twitter.com/devhasibulislam
+ * Pinterest: https://pinterest.com/devhasibulislam
+ * WhatsApp: https://wa.me/8801906315901
+ * Telegram: devhasibulislam
+ * Date: 20, January 2024
+ */
+
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  products: [],
+  product: {},
+};
+
+const productSlice = createSlice({
+  name: "product",
+  initialState,
+  reducers: {
+    setProducts: (state, action) => {
+      state.products = action.payload;
+    },
+    setProduct: (state, action) => {
+      state.product = action.payload;
+    },
+  },
+});
+
+export const { setProducts, setProduct } = productSlice.actions;
+export default productSlice.reducer;
