483e142d68777a9945074a136c702c824ecc4f86
devhasibulislam <devhasibulislam@gmail.com>
2024-01-21 00:57:34 +0600

    integrate logout button at sidebar

--- client/src/components/shared/layouts/Dashboard.js
+++ client/src/components/shared/layouts/Dashboard.js
@@ -274,26 +274,39 @@ const Dashboard = ({ children }) => {
                     </div>
                   </div>
                 ))}
-                <Link
-                  href="/"
-                  className="text-sm bg-slate-50/50 p-2 rounded mt-auto flex flex-row gap-x-1 items-center"
-                >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    strokeWidth={1.5}
-                    stroke="currentColor"
-                    className="w-5 h-5"
+                <p className="flex flex-row justify-between items-center">
+                  <Link
+                    href="/"
+                    className="text-sm bg-slate-50/50 p-2 rounded mt-auto flex flex-row gap-x-1 items-center"
                   >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"
-                    />
-                  </svg>
-                  Go to Home
-                </Link>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth={1.5}
+                      stroke="currentColor"
+                      className="w-5 h-5"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"
+                      />
+                    </svg>
+                    Go to Home
+                  </Link>
+                  <button
+                    type="button"
+                    className="p-0.5 rounded-secondary border block md:hidden"
+                    title="Logout"
+                    onClick={() => {
+                      localStorage.removeItem("accessToken");
+                      window.open("/", "_self");
+                    }}
+                  >
+                    <Logout />
+                  </button>
+                </p>
               </div>
             </div>
           )}
