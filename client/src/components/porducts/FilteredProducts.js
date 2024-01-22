1e169c58070ff82857a1a8c83afd9f50edbfb262
devhasibulislam <devhasibulislam@gmail.com>
2024-01-22 13:25:59 +0600

    integrate filtered product

--- client/src/components/porducts/FilteredProducts.js
+++ client/src/components/porducts/FilteredProducts.js
@@ -21,32 +21,67 @@ import {
   useGetFilteredProductsMutation,
   useGetFilteredProductsQuery,
 } from "@/services/product/productApi";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import ProductCard from "../shared/skeletonLoading/ProductCard";
+import { useSearchParams } from "next/navigation";
+import { toast } from "react-hot-toast";
+import { setBrand, setCategory, setStore } from "@/features/filter/filterSlice";
 
 const FilteredProducts = () => {
-  const { filterCredentials } = useSelector((state) => state.productFilter);
+  const filter = useSelector((state) => state.filter);
   const [
     addFilter,
     { data: productsData, error: productsError, isLoading: productsLoading },
   ] = useGetFilteredProductsMutation();
   const products = useMemo(() => productsData?.data || [], [productsData]);
 
-  useEffect(() => {
-    addFilter(filterCredentials);
-  }, [filterCredentials, addFilter]);
+  const dispatch = useDispatch();
+  const searchParams = useSearchParams();
+  const brand = searchParams.get("brand");
+  const category = searchParams.get("category");
+  const store = searchParams.get("store");
 
   useEffect(() => {
-    if (productsError) {
-      alert(productsError?.data?.description);
+    addFilter(new URLSearchParams(filter).toString());
+  }, [filter, addFilter]);
+
+  useEffect(() => {
+    if (productsLoading) {
+      toast.loading("Loading...", {
+        id: "filtered-products",
+      });
     }
-  }, [productsError]);
+
+    if (productsData) {
+      toast.success(productsData?.description, {
+        id: "filtered-products",
+      });
+    }
+
+    if (productsError?.data) {
+      toast.error(productsError?.data?.description, {
+        id: "filtered-products",
+      });
+    }
+
+    if (brand) dispatch(setBrand(brand));
+    if (category) dispatch(setCategory(category));
+    if (store) dispatch(setStore(store));
+  }, [
+    productsError,
+    productsData,
+    productsLoading,
+    brand,
+    category,
+    store,
+    dispatch,
+  ]);
 
   return (
     <div className="lg:col-span-9 md:col-span-8 col-span-12">
       <div className="flex flex-col gap-y-8">
         <div className="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 md:gap-x-6 gap-y-8">
-          {productsLoading || products?.length === 0 ? (
+          {productsLoading ? (
             <>
               {[1, 2, 3, 4, 5, 6].map((_, index) => (
                 <ProductCard key={index} />
@@ -60,6 +95,9 @@ const FilteredProducts = () => {
             </>
           )}
         </div>
+        {!productsLoading && products?.length === 0 && (
+          <p className="text-center">Oops! No products found!</p>
+        )}
       </div>
     </div>
   );
