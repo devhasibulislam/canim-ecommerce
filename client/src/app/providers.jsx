55f2ed1fa577b0c69c0473f1f2e5114d53488e4f
devhasibulislam <devhasibulislam@gmail.com>
2024-01-23 13:25:16 +0600

    modify cursor animation

--- client/src/app/providers.jsx
+++ client/src/app/providers.jsx
@@ -18,11 +18,17 @@
 import { Provider } from "react-redux";
 import { store } from "./store";
 import React from "react";
+import AnimatedCursor from "react-animated-cursor";
 
 const Providers = ({ children }) => {
   return (
     <Provider store={store}>
       {children}
+      <AnimatedCursor
+        innerSize={10}
+        color="0, 0, 0"
+        showSystemCursor={false}
+      />
     </Provider>
   );
 };
